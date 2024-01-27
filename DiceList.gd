d908a7a8f68e61be788c15306e37b86d802f603e
DoctorEaker <kuiser1992@gmail.com>
2024-01-27 06:13:04 -0500

    dice base logic
    Signed-off-by: DoctorEaker <kuiser1992@gmail.com>

---
+++ DiceList.gd
@@ -0,0 +1,9 @@
+extends Resource
+
+var Dice = preload("res://Dice.gd")
+@export var dice_list = []
+
+func _init(p_dice_list = [Dice.new(0.5,0.5), Dice.new(0.7,0.3,2), Dice.new(0.3,0.7,2)]):
+	dice_list = p_dice_list
+	
+
